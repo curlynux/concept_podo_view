00e7a855888ed0075140f1845fbb27b0d27add67
curlynux <smpunchack@gmail.com>
2017-10-07 17:26:34 +0200

    bs array

---
+++ index.js
@@ -0,0 +1,20 @@
+var value = [
+  'Table FORME',
+    "Longueur de la chaussure (numérique, en cm)",
+    "Largeur des flancs (au sol) (numérique, en cm)",
+    "Tour de la ligne des flancs (numérique, en cm)",
+    "Tour du coup de pied (numérique, en cm)",
+    "Entrée (numérique, en cm)",
+    "Hauteur 10 (numérique, en cm)",
+    "Hauteur 15 (numérique, en cm)",
+    "Largeur du talon (optionnel) (numérique, en cm)",
+
+    "Nom du fichier de la « forme numérique »",
+    "Chemin du fichier de la « forme numérique »",
+    "Est-ce qu’il existe une « forme en bois » ?",
+    "Est-ce qu’il y a une retouche sur la forme en bois ?",
+
+    "User qui a créé la forme",
+    "Date de création de la forme",
+    "Date de dernière mise à jour de la forme"
+]
