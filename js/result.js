0deb1524aabbb6c8e1ee61ff58f8e5114fe19a91
curlynux <smpunchack@gmail.com>
2017-10-08 12:29:47 +0200

    fake commit

--- js/result.js
+++ js/result.js
@@ -1,7 +1,7 @@
 (function() {
   // Load the script
   var script = document.createElement("SCRIPT");
-  script.src = 'https://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js';
+  script.src = 'https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js';
   script.type = 'text/javascript';
   script.onload = function() {
     var $ = window.jQuery;
@@ -53,6 +53,7 @@
     }*/
   }
 }
+console.log(form);
 form.get();
 };
 document.getElementsByTagName("head")[0].appendChild(script);
